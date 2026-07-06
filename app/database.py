5a863a7bd19faff1429626dcdfd7712ba307a2d7
Pro100Maks88 <pro100maks88@yandex.ru>
2026-07-06 23:34:54 +0300

    updated file database.py

--- app/database.py
+++ app/database.py
@@ -1,24 +1,24 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlmodel import Session, SQLModel
 
+
+if os.path.exists(".env"):
+    load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 if not DATABASE_URL:
     raise RuntimeError(
-        "DATABASE_URL не задан. Переменная должна быть передана через environment "
-        "(например, в docker-compose.yml), а не через .env файл."
+        "DATABASE_URL не задан. Создай .env файл или проверь окружение."
     )
 
-engine = create_engine(
-    DATABASE_URL,
-    echo=False,
-    future=True,
-)
+engine = create_engine(DATABASE_URL, echo=False, future=True)
 
 def init_db():
     SQLModel.metadata.create_all(engine)
-
 
 def get_session():
     with Session(engine) as session:
